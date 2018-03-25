6e05ae1355ed281b78247c4ec7d5834a32a87fa3
LamineMbn <octave.wolfy@gmail.com>
2018-03-25 19:40:36 +0200

    Add dataSet utils, to retrieve data

---
+++ src/main/java/com/sample/io/utils/DataSetUtils.java
@@ -0,0 +1,34 @@
+package com.sample.io.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataSetUtils {
+
+	private static final int RULES_INDEX = 0;
+
+	public static List<String> retrieveRulesFromDataSet(List<String> dataSet) {
+		return Arrays.asList(dataSet.get(RULES_INDEX).split(StringUtils.SPACE));
+	}
+
+	public static List<String> retrieveDataFromDataSet(List<String> dataSet) {
+		return dataSet.subList(RULES_INDEX + 1, dataSet.size());
+	}
+
+	public static Integer[][] generateMatrixFromList(List<String> dataList, int numberOfRow, int numberOfColumn) {
+		Integer[][] matrix = new Integer[numberOfRow][numberOfColumn];
+		int i = 0;
+		int j = 0;
+		for (String row : dataList) {
+			for (String col : row.split(StringUtils.SPACE)) {
+					matrix[i][j] = new Integer(col);
+				j++;
+			}
+			i++;
+			j = 0;
+		}
+		return matrix;
+	}
+}
---
+++ src/main/java/com/sample/io/utils/FileNames.java
@@ -0,0 +1,8 @@
+package com.sample.io.utils;
+
+public class FileNames {
+
+	public static String INPUT_1 = "input1.txt";
+	public static String INPUT_2 = "input2.txt";
+	public static String INPUT_3 = "input3.txt";
+}
